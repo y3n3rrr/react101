6ecc790e547e70640f4fe3f848e13968ee72f6b4
Mehmet Yener YILMAZ <yilmazmehmetyener@gmail.com>
2025-02-01 12:11:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div className="container">
+  <div className="row justify-content-center mt-5">
+    <div className="col-lg-4 col-md-6 col-sm-6">
+      <div className="card shadow">
+        <div className="card-title text-center border-bottom">
+          <h2 className="p-3">KAYIT</h2>
+        </div>
+        <div className="card-body">
+          <form>
+            <div className="mb-4">
+              <label htmlFor="username" className="form-label">
+                Kullanıcı Adı
+              </label>
+              <input type="text" className="form-control" id="username" />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="form-label">
+                Şifre
+              </label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+            <div className="mb-4">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="remember"
+              />
+              <label htmlFor="remember" className="form-label">
+                Beni Hatırla
+              </label>
+            </div>
+            <div className="d-grid">
+            
+              <button type="button" class="btn btn-secondary">GİRİŞ</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
+  </div>
+</div>
+
+
   );
 }
 
