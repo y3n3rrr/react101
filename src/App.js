8b82efebb3f56e65aa516a2493b08e9766c15c4a
Mehmet Yener YILMAZ <yilmazmehmetyener@gmail.com>
2025-03-01 12:33:22 +0300

    google sing in

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import Login from './Login'
 import Profile from './Profile'
 import Layout from './Layout'
 import { AuthProvider } from './hooks/AuthContext'
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   return (
   <BrowserRouter>
+  <GoogleOAuthProvider clientId='332558980603-vj86b6v11eua2o48eo66qfrq98ojmkat.apps.googleusercontent.com'>
     <AuthProvider>
         <Routes>
           <Route path='/login' element={<Login />} />
@@ -21,6 +22,7 @@ function App() {
             </Route>
         </Routes>
     </AuthProvider>
+    </GoogleOAuthProvider>
   </BrowserRouter>
   )
 }
